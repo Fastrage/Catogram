60d2bbaebc78adfb89cd2d79d422562be593ff7e
Oleg Krylov <Fastrage@mail.ru>
2019-08-02 19:31:51 +0700

    Initial Commit

---
+++ CatogramTests/CatogramTests.swift
@@ -0,0 +1,34 @@
+//
+//  CatogramTests.swift
+//  CatogramTests
+//
+//  Created by Олег Крылов on 02/08/2019.
+//  Copyright © 2019 OlegKrylov. All rights reserved.
+//
+
+import XCTest
+@testable import Catogram
+
+class CatogramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
